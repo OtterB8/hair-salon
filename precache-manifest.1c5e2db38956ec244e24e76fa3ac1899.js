896072f50a2491bf1cac776e861fecebdbcf0245
OtterB8 <caberrrr@gmail.com>
2020-02-05 12:16:01 +0700

    Updates

---
+++ precache-manifest.1c5e2db38956ec244e24e76fa3ac1899.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab48856fda9643315125809d635037d4",
+    "url": "/hair-salon/index.html"
+  },
+  {
+    "revision": "7a2a59a443defc52dfe7",
+    "url": "/hair-salon/static/css/main.64a1e83e.chunk.css"
+  },
+  {
+    "revision": "6355a4f6e982f0219c3c",
+    "url": "/hair-salon/static/js/2.0ce7ee47.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/hair-salon/static/js/2.0ce7ee47.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a2a59a443defc52dfe7",
+    "url": "/hair-salon/static/js/main.5b315fe9.chunk.js"
+  },
+  {
+    "revision": "167ee466ad1cc95c5fbe",
+    "url": "/hair-salon/static/js/runtime-main.42ddc2a1.js"
+  },
+  {
+    "revision": "7851f9978647488ed739f4c6ab646eb5",
+    "url": "/hair-salon/static/media/Subtle-Prism.7851f997.svg"
+  },
+  {
+    "revision": "47aa20467460e02e87379db08d042047",
+    "url": "/hair-salon/static/media/loading.47aa2046.svg"
+  },
+  {
+    "revision": "b40e9d4e2c32340db1c314b79cea52f4",
+    "url": "/hair-salon/static/media/scissors.b40e9d4e.jpg"
+  },
+  {
+    "revision": "95887f542feed79ac5ce4d666c6ef8c0",
+    "url": "/hair-salon/static/media/up-arrow.95887f54.svg"
+  }
+]);
