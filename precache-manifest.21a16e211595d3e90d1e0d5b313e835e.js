e6b2aad9f6ce616fd423395f92df3d70143c6292
OtterB8 <caberrrr@gmail.com>
2020-02-05 15:08:54 +0700

    Updates

---
+++ precache-manifest.21a16e211595d3e90d1e0d5b313e835e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "925baaba87c7b0c7c39529f86064b828",
+    "url": "/hair-salon/index.html"
+  },
+  {
+    "revision": "342ac19b503ce7e0a2bd",
+    "url": "/hair-salon/static/css/main.64a1e83e.chunk.css"
+  },
+  {
+    "revision": "bd3d280b9b9f0d77bad9",
+    "url": "/hair-salon/static/js/2.f522b633.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/hair-salon/static/js/2.f522b633.chunk.js.LICENSE"
+  },
+  {
+    "revision": "342ac19b503ce7e0a2bd",
+    "url": "/hair-salon/static/js/main.249630fc.chunk.js"
+  },
+  {
+    "revision": "167ee466ad1cc95c5fbe",
+    "url": "/hair-salon/static/js/runtime-main.42ddc2a1.js"
+  },
+  {
+    "revision": "7851f9978647488ed739f4c6ab646eb5",
+    "url": "/hair-salon/static/media/Subtle-Prism.7851f997.svg"
+  },
+  {
+    "revision": "47aa20467460e02e87379db08d042047",
+    "url": "/hair-salon/static/media/loading.47aa2046.svg"
+  },
+  {
+    "revision": "b40e9d4e2c32340db1c314b79cea52f4",
+    "url": "/hair-salon/static/media/scissors.b40e9d4e.jpg"
+  },
+  {
+    "revision": "95887f542feed79ac5ce4d666c6ef8c0",
+    "url": "/hair-salon/static/media/up-arrow.95887f54.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60b59dda8d937915bcd7d2b5aeb1d036.js"
+  "/hair-salon/precache-manifest.21a16e211595d3e90d1e0d5b313e835e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hair-salon/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
