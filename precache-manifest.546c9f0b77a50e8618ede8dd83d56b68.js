fb101c04b38fc163abffd71303ebceb57da9c12c
OtterB8 <caberrrr@gmail.com>
2020-02-19 21:48:26 +0700

    Updates

---
+++ precache-manifest.546c9f0b77a50e8618ede8dd83d56b68.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "666dba8a6a9e87f55837ae4a5a01608a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e011249b447fe9fbf312",
+    "url": "/static/css/main.373b3634.chunk.css"
+  },
+  {
+    "revision": "bd3d280b9b9f0d77bad9",
+    "url": "/static/js/2.f522b633.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/static/js/2.f522b633.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e011249b447fe9fbf312",
+    "url": "/static/js/main.72a3a6f4.chunk.js"
+  },
+  {
+    "revision": "38e3a03e83c9b6a179f1",
+    "url": "/static/js/runtime-main.c3c7c125.js"
+  },
+  {
+    "revision": "7851f9978647488ed739f4c6ab646eb5",
+    "url": "/static/media/Subtle-Prism.7851f997.svg"
+  },
+  {
+    "revision": "47aa20467460e02e87379db08d042047",
+    "url": "/static/media/loading.47aa2046.svg"
+  },
+  {
+    "revision": "b40e9d4e2c32340db1c314b79cea52f4",
+    "url": "/static/media/scissors.b40e9d4e.jpg"
+  },
+  {
+    "revision": "95887f542feed79ac5ce4d666c6ef8c0",
+    "url": "/static/media/up-arrow.95887f54.svg"
+  }
+]);
